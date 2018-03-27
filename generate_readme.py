54f4036bb44e9a662cb83ea8506152dad3ca0678
Yadkee <yadkee@gmail.com>
2018-03-27 04:50:42 +0200

    Created folder files and adaptated generate_readme

--- generate_readme.py
+++ generate_readme.py
@@ -2,9 +2,9 @@
 from os import listdir
 from os.path import join
 
-with open("readme_preset.txt", "rb") as f:
+with open(join("files", "readme_preset.txt"), "rb") as f:
     preset = f.read()
-with open("index.txt") as f:
+with open(join("files", "index.txt")) as f:
     index = f.read().splitlines()
 text = []
 for i, j in zip(range(1, 1000, 25), range(25, 1000, 25)):
@@ -12,12 +12,12 @@ for i, j in zip(range(1, 1000, 25), range(25, 1000, 25)):
     problemPath = join(".", segment)
     githubPath = "/%s/problem" % segment
     try:
-        problems = tuple(sorted(listdir(path=problemPath), reverse=True))
+        problems = tuple(sorted(listdir(path=problemPath), reverse=False))
     except FileNotFoundError:
         break
-    for a, path in enumerate(problems):
-        n = a + i
-        text.append("- [%s](%s%03d.py)" % (index[n], githubPath, n))
+    for path in problems:
+        n = int(path.strip(".py")[-3:])
+        text.append("- [%s](%s%s)" % (index[n], githubPath, path))
 
 with open("README.md", "wb") as f:
     f.write(preset % ("\n".join(text)).encode())
