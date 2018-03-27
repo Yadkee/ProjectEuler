ede19ee5bf1a2ef39d2f23eb4e3c52874d825366
Yadkee <yadkee@gmail.com>
2018-03-27 21:42:48 +0200

    Added problem 42

--- problems[026-050]/problem042.py
+++ problems[026-050]/problem042.py
@@ -12,5 +12,5 @@ def value(word):
     return sum(ABC.index(i) for i in word) + len(word)
 
 # t = n(n + 1) / 2  -->  n = (-1 + sqrt(1 + 8t)) / 2
-# So if n is an integer then sqrt(1 + 8t) - 1 has gotta be even (and int)
+# So if n is an integer then 'sqrt(1 + 8t) - 1' needs to be even (and int)
 print(sum(1 for i in words if (sqrt(1 + 8 * value(i)) - 1) % 2 == 0))
