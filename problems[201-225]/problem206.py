c4a7c3d7dea7883df15f626f9c3df46f97c6b782
Yadkee <yadkee@gmail.com>
2018-04-23 23:43:32 +0200

    Optimized times by being smarter

--- problems[201-225]/problem206.py
+++ problems[201-225]/problem206.py
@@ -3,6 +3,7 @@
 1_2_3_4_5_6_7_8_9_0, where each “_” is a single digit."""
 from math import sqrt
 from time import time
+from itertools import cycle
 
 # If it ends with 0 the squared must end with 0 too
 # If (removing the 0s) it ends with 9 then the squared end must be 3 or 7
@@ -11,19 +12,11 @@ mx = int(sqrt(19293949596979899))
 s = "123456789"
 
 
-def meets_condition(n):
-    return str(n)[::2] == s
-
-# This will return the correct value (138******0) in 10 seconds
+# This will return the correct value (138******0) in 5 seconds
 t0 = time()
-i = mn + 2  # So the first value ends with 3
-t = 0  # Keeps track of whether we are on a 3 or a 7-ended number
-while i < mx:
-    if meets_condition(i ** 2):
+i = mn + mn % 3  # So the first value ends with 3
+for t in cycle((4, 6)):  # ***3 -> ***7 -> ***3 -> ...
+    if str(i * i)[::2] == s:
         print(i * 10, "in %d seconds" % int(time() - t0))
         break
-    if t:  # From ***7 to ***3
-        i += 6
-    else:  # From ***3 to ***7
-        i += 4
-    t = not t
+    i += t
