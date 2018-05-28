8bc96464289afdf4259dfc83c1203af3baa890e4
Yadkee <yadkee@gmail.com>
2018-05-29 01:18:06 +0200

    Improved problem 46

--- problems[026-050]/problem046.py
+++ problems[026-050]/problem046.py
@@ -6,14 +6,13 @@ from math import sqrt
 import sys
 from os.path import dirname
 sys.path.insert(0, dirname(dirname(__file__)))
-from utils import primes_until
 from utils import is_prime
 
 for i in count(3, 2):
     if not sqrt(i / 2) % 1 or is_prime(i):
         continue
-    for j in primes_until(i):
-        if not sqrt((i - j) / 2) % 1:
+    for j in range(1, i):
+        if is_prime(i - 2 * j * j):
             break
     else:
         print(i)
