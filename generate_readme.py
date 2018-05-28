e4fb944471dc9d228361cda10e7a5be6242e989c
Yadkee <yadkee@gmail.com>
2018-05-29 01:07:07 +0200

    Moved from 3 columns to 2

--- generate_readme.py
+++ generate_readme.py
@@ -2,11 +2,13 @@
 from os import listdir
 from os.path import join
 
+N = 2
+
 with open(join("files", "readme_preset.txt"), "rb") as f:
     preset = f.read()
 with open(join("files", "index.txt")) as f:
     index = f.read().splitlines()
-cells = [[[". " * 30] * 25 for c in range(3)] for r in range(8)]
+cells = [[[". " * 30] * 25 for c in range(N)] for r in range(24 // N)]
 problemFolders = sorted((i for i in listdir(path=".")
                          if i.startswith("problems[")), reverse=False)
 for folder in problemFolders:
@@ -15,7 +17,7 @@ for folder in problemFolders:
     problems = sorted(listdir(path=problemPath), reverse=False)
     for path in problems:
         n = int(path.strip(".py")[-3:]) - 1
-        r, c = (n // 25) // 3, (n // 25) % 3
+        r, c = (n // 25) // N, (n // 25) % N
         cells[r][c][n % 25] = "[%s](%s%s)" % (index[n + 1], githubPath, path)
 
 
@@ -24,7 +26,8 @@ def html_list(l):
 
 
 def html_table(l):
-    return "<tr>%s</tr>" % "".join("<td width=33%%>\n\n%s</td>" % i for i in l)
+    td = "<td width=%d%s>\n\n%s</td>" % (100 // N, "%%", "%s")
+    return "<tr>%s</tr>" % "".join(td % i for i in l)
 
 table = "\n".join(html_table(html_list(item) for item in row)
                   for row in cells if row != cells[-1]).encode()
