44ad1d4d87390abcf2393028adaacc7b11e7c0a6
Yadkee <yadkee@gmail.com>
2018-06-13 23:38:46 +0200

    Improved problem 58

--- problems[051-075]/problem058.py
+++ problems[051-075]/problem058.py
@@ -11,11 +11,11 @@ from utils import is_prime
 t0 = time()
 primes = 0
 v = 1
-for i in count(1):  # Takes around 13 seconds
-    for j in range(4):
-        v += 2 * i
+for i in count(2, 2):  # Takes around 11 seconds
+    for _ in range(4):
+        v += i
         if is_prime(v):
-            primes += 100
-    if primes // (4 * i + 1) < 10:
-        print(2 * i + 1, "in %.2f seconds" % (time() - t0))
+            primes += 10
+    if not primes // (i + i + 1):
+        print(i + 1, "in %.2f seconds" % (time() - t0))
         break
