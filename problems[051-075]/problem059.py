177d28c39142a22686b48b13ae1fbd3288b31de4
Yadkee <yadkee@gmail.com>
2018-06-14 03:56:23 +0200

    Added description to problem 59

--- problems[051-075]/problem059.py
+++ problems[051-075]/problem059.py
@@ -1,4 +1,9 @@
 #! python3
+"""Your task has been made easy, as the encryption key consists of three lower
+case characters. Using cipher.txt (right click and 'Save Link/Target As...'),
+a file containing the encrypted ASCII codes, and the knowledge that the plain
+text must contain common English words, decrypt the message and find the sum
+of the ASCII values in the original text."""
 from os.path import join
 from string import ascii_lowercase
 from collections import Counter
