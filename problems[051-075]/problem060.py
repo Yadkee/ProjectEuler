48d97b45ca4e177b518787a0c6a6c79759e634d9
Yadkee <yadkee@gmail.com>
2018-06-14 05:19:26 +0200

    Problem 60 is no longer a big spaghetti bowl :D

--- problems[051-075]/problem060.py
+++ problems[051-075]/problem060.py
@@ -1,40 +1,27 @@
 #! python3
 """Find the lowest sum for a set of five primes for which any two primes
 concatenate to produce another prime."""
-from functools import lru_cache
 from time import time
 import sys
 from os.path import dirname
 sys.path.insert(0, dirname(dirname(__file__)))
 from utils import is_prime, prime_generator
-is_prime = lru_cache(maxsize=1 << 16)(is_prime)
 
-# I am sorry for the spaghetti code
 t0 = time()
-primes = []
 pairs = dict()
-for i in map(str, prime_generator()):  # It can take almost a minute
-    own = set()
-    own.add(i)
-    for j in primes:
-        if is_prime(int(i + j)) and is_prime(int(j + i)):
-            try:
-                pairs[j].add(i)
-            except KeyError:
-                pairs[j] = set((j, i))
-            own.add(j)
+for i in map(str, prime_generator()):  # It takes ~45 seconds
+    own = set(j for j in pairs if
+              is_prime(int(i + j)) and is_prime(int(j + i)))
     for j in own:
-        if j == i:
-            continue
-        for k in own - set((i, j)):
-            for l in own - set((i, j, k)):
-                inter = pairs[j] & pairs[k] & pairs[l] & own
-                if len(inter) == 4:  # This is only to feel that sthg happens
-                    print(".", end="", flush=True)
-                elif len(inter) == 5:
-                    print("\n\n\n\n", inter, sep="")
-                    print(sum(map(int, inter)))
+        pj = pairs[j] & own
+        for k in pj:
+            pk = pairs[k] & pj
+            for l in pk:
+                print(".", end="", flush=True)
+                for m in pairs[l] & pk:
+                    print("\n\n")
+                    print(i, j, k, l, m)
+                    print(sum(map(int, (i, j, k, l, m))))
                     print("In %d seconds" % (time() - t0))
                     raise SystemExit
     pairs[i] = own
-    primes.append(i)
