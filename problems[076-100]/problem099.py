67178f51ba73c2675761bf6690ab381ee70a8afb
Yadkee <yadkee@gmail.com>
2018-06-16 02:50:04 +0200

    Added description to problem 99

--- problems[076-100]/problem099.py
+++ problems[076-100]/problem099.py
@@ -1,9 +1,12 @@
 #! python3
+"""Using base_exp.txt, a 22K text file containing one thousand lines with a
+base/exponent pair on each line, determine which line number has the greatest
+numerical value."""
 from os.path import join
 from math import log10
 
 with open(join("..", "files", "p099_base_exp.txt")) as f:
-    numbers = [map(int, i.split(",")) for i in f.readlines()]
+    numbers = (map(int, i.split(",")) for i in f.readlines())
 
 out = ((exp * log10(base), a) for a, (base, exp) in enumerate(numbers))
 print(max(out)[1] + 1)  # "+1": indexes should start at 0 but here they do not.
