e5e536b28926069fb723836dd44101f8b0bec2f5
Yadkee <yadkee@gmail.com>
2018-06-16 02:41:56 +0200

    "Improved" problem 99

--- problems[076-100]/problem099.py
+++ problems[076-100]/problem099.py
@@ -3,10 +3,7 @@ from os.path import join
 from math import log10
 
 with open(join("..", "files", "p099_base_exp.txt")) as f:
-    numbers = [i.split(",") for i in f.readlines()]
+    numbers = [map(int, i.split(",")) for i in f.readlines()]
 
-out = []
-for a, i in enumerate(numbers):
-    base, exp = map(int, i)
-    out.append((exp * log10(base), a))
+out = ((exp * log10(base), a) for a, (base, exp) in enumerate(numbers))
 print(max(out)[1] + 1)  # "+1": indexes should start at 0 but here they do not.
